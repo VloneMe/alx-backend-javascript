9d0af198a1b653b1fd3628e5ac67070904ad0c81
Vlone Me <francisntima@gmail.com>
2024-01-04 01:32:24 +0000

    Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,7 @@
 export default function createIteratorObject(report) {
   return (function* _() {
-
     // This eslint-disables-next-line no-restricted-syntax
     for (const department of Object.values(report.allEmployees)) {
-
       // This eslint-disables-next-line no-restricted-syntax
       for (const employee of department) {
         yield employee;

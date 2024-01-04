009e3e3e6b617bb11e249854c68d22b33c7da81f
Vlone Me <francisntima@gmail.com>
2024-01-04 02:12:58 +0000

    Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+// Await / Async
+
+import { uploadPhoto, createUser } from './utils';
+
+const asyncUploadUser = async () => {
+  try {
+    return { photo: await uploadPhoto(), user: await createUser() };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+};
+
+export default asyncUploadUser;

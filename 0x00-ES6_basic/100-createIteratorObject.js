d41759991674d518e1208b9a248bdba2c395f6cc
Vlone Me <francisntima@gmail.com>
2024-01-04 00:59:11 +0000

    Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,26 @@
+export default function createIteratorObject(report) {
+  const departments = Object.values(report.allEmployees);
+  let currentDepartmentIndex = 0;
+  let currentEmployeeIndex = 0;
+
+  return {
+    next: function () {
+      if (currentDepartmentIndex < departments.length) {
+        const currentDepartment = departments[currentDepartmentIndex];
+        if (currentEmployeeIndex < currentDepartment.length) {
+          const employee = currentDepartment[currentEmployeeIndex];
+          currentEmployeeIndex++;
+          return { value: employee, done: false };
+        } else {
+          // Move to the next department
+          currentDepartmentIndex++;
+          currentEmployeeIndex = 0;
+          return this.next(); // Recursive call for the next department
+        }
+      } else {
+        // No more departments or employees
+        return { done: true };
+      }
+    },
+  };
+}

ef454c43d746d5f914aa4c688f7a9985444c8596
Vlone Me <francisntima@gmail.com>
2024-01-04 15:49:21 +0000

    Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+// Inheritance
+
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate the building calmly, considering its ${this.floors} floors`;
+  }
+}

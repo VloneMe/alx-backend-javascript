2d38ae4058d84a3284477068d77e48429dea6b19
Vlone Me <francisntima@gmail.com>
2024-01-05 07:28:24 +0000

    Build

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,4 +1,12 @@
+/**
+ * Building class represents an abstract building with square footage.
+ * Subclasses must extend this class and provide an implementation for the evacuationWarningMessage method.
+ */
 export default class Building {
+  /**
+   * Constructor for the Building class.
+   * @param {number} sqft - The square footage of the building.
+   */
   constructor(sqft) {
     // Ensure that the class is not instantiated directly
     if (this.constructor === Building) {
@@ -6,7 +14,8 @@ export default class Building {
     }
 
     // Check if the extended class overrides evacuationWarningMessage
-    if (!('evacuationWarningMessage' in this.constructor.prototype)) {
+    const props = Object.getOwnPropertyNames(this.constructor.prototype);
+    if (!props.find((e) => e === 'evacuationWarningMessage')) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
@@ -14,14 +23,21 @@ export default class Building {
     this._sqft = sqft;
   }
 
-  // Getter for sqft
+  /**
+   * Getter for the square footage of the building.
+   * @returns {number} - The square footage.
+   */
   get sqft() {
     return this._sqft;
   }
 
-  // Setter for sqft with type check
+  /**
+   * Setter for the square footage of the building.
+   * @param {number} value - The new square footage value.
+   * @throws {TypeError} - Throws an error if the provided value is not a number.
+   */
   set sqft(value) {
-    if (typeof value !== 'number' && !(value instanceof Number)) {
+    if (typeof value !== 'number') {
       throw new TypeError('Sqft must be a number');
     }
     this._sqft = value;

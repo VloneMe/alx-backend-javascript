bcc926c601f00a7c6880fec694be646e6421e811
Vlone Me <francisntima@gmail.com>
2024-01-05 06:54:03 +0000

    EVCar update

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,11 +1,11 @@
-import Car from './10-car.js';
+iimport Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
-  
+
   cloneCar() {
     const clone = new (
       Object.getPrototypeOf(this.constructor))(this._brand, this._motor, this._color);
